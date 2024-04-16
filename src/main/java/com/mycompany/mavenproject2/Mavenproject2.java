0bdf01247ed5183021281c0b0f8da159e1a4a225
gribu4ok <162813176+gribu4ok@users.noreply.github.com>
2024-04-16 16:33:02 +0200

    Laba3 add

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author gribu
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        TimerFrame f = new TimerFrame ();
+        f.setVisible(true);
+    }
+}
